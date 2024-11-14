8e1150be60ac1ca900b2ed2e57b0f18eb6245726
Josue Segura <jsegura@fi.uba.ar>
2024-11-14 20:33:26 -0300

    Arreglos al header

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,37 +6,37 @@ app = Flask(__name__)
 
 def home():
 
-    return render_template('home.html')
+    return render_template('home.html', endpoint=request.endpoint)
 
 @app.route('/NuestrosHoteles')
 
 def NuestrosHoteles():
 
-    return render_template("NuestrosHoteles.html")
+    return render_template("NuestrosHoteles.html", endpoint=request.endpoint)
 
 @app.route('/Galeria')
 
 def Galeria():
 
-    return render_template("Galeria.html")
+    return render_template("Galeria.html", endpoint=request.endpoint)
 
 @app.route('/Reservas')
 
 def Reservas():
 
-    return render_template("Reservas.html")
+    return render_template("Reservas.html", endpoint=request.endpoint)
 
 @app.route('/ConsultaReserva')
 
 def ConsultaReserva():
 
-    return render_template("ConsultaReserva.html")
+    return render_template("ConsultaReserva.html", endpoint=request.endpoint)
 
 @app.route('/contact')
 
 def contact():
 
-    return render_template("contact.html")
+    return render_template("contact.html", endpoint=request.endpoint)
 
 if __name__ == "__main__":
     app.run("127.0.0.1", port="8080", debug=True)
