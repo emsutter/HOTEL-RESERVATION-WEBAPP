022d824f322a07406b6df47a4c883c74aad747fd
Ezequiel Medina <esutter@ims-company.com>
2024-11-28 15:57:17 -0300

    cambios: formato de app.py

--- front/app.py
+++ front/app.py
@@ -1,29 +1,23 @@
 from flask import Flask, jsonify, request, render_template
 from flask_sqlalchemy import SQLAlchemy
-import consultas
 from flask_mail import Mail, Message
 from flask_cors import CORS, cross_origin
 from flask import session
 from flask import redirect
-import os
-#from dotenv import load_dotenv
-
-#load_dotenv()  # Carga las variables de entorno desde .env
 from flask import session
 from flask import redirect
+import consultas
+import os
+
 app = Flask(__name__)
 
-CORS(app)  # Habilita CORS para todas las rutas
-
-from datetime import timedelta
-
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)    
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://marm4:Moqnit-1dakte-dikbew@marm4.mysql.pythonanywhere-services.com/marm4$apc_db'
+CORS(app)  # Habilita CORS para todas las rutas 
 
 from datetime import timedelta
 
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://marm4:Moqnit-1dakte-dikbew@marm4.mysql.pythonanywhere-services.com/marm4$apc_db'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -47,14 +41,13 @@ def get_google_maps_api_key():
 @app.route('/')
 def home():
     imagenes = consultas.obtener_imagenes()
+    
     return render_template('home.html', imagenes=imagenes, endpoint=request.endpoint)
 
-
 @app.route('/prueba')
-
 def prueba():
     prueba = session.get('reserva', [])
-    
+
     return render_template("pruebas.html", prueba=prueba)
 
 @app.route('/cancelar_reserva/<int:id>', methods = ['GET','POST']) 
@@ -62,36 +55,39 @@ def cancelar_reserva(id):
     reserva = consultas.obtener_reseva_por_id(id)
     hotel_id = reserva[0]['hotel_id']   #recordar que se deshabilita cuando es 0 (falta que se vaya de la pestania consultas )
     hotel = consultas.obtener_hotel_por_id(hotel_id)
-
+    
     if request.method == 'POST':
         borrar_reserva(id)
         return redirect('/mis_reservas')
+    
     return render_template('cancelacion.html', reserva = reserva[0], hotel = hotel)
 
 @app.route('/NuestrosHoteles')
 def NuestrosHoteles():
     hoteles = consultas.obtener_hoteles_con_imagen()
+    
     return render_template("NuestrosHoteles.html", hoteles=hoteles, endpoint=request.endpoint)
 
 @app.route('/Galeria')
 def Galeria():
     imagenes = consultas.obtener_imagenes()
+    
     return render_template("Galeria.html", imagenes=imagenes, endpoint=request.endpoint)
 
 @app.route('/Reservas', methods = ['GET', 'POST'])
 def Reservas():
     hoteles = consultas.obtener_hoteles()
     hotel_id = request.args.get('hotel_id')
-
+    
     return render_template("Reservas.html", hoteles=hoteles, hotel_id=hotel_id, endpoint=request.endpoint)
 
 def appendear_reservas_session(reservas):
     if "reserva" not in session:
         session['reserva'] = []
+        
     for reserva in reservas:
         if reserva not in session['reserva']:
             session['reserva'].append(reserva)
-
 
 @app.route('/ConsultaReserva', methods=['GET', 'POST'])
 def ConsultaReserva():
@@ -107,15 +103,16 @@ def ConsultaReserva():
         else:
             error = f"mail incorrecto {str(reservas_por_usuario[1])}"
             return render_template("ConsultaReserva.html", error=error)
-
+        
     if not 'email' in session:
         return render_template("ConsultaReserva.html")
+    
     return redirect('/mis_reservas')    
 
 @app.route('/mis_reservas')
 def mis_reservas():
     mail = session.get('email')
-
+    
     if  mail:
         reservas = session.get('reserva', [])
         return render_template("mis_reservas.html", reservas = reservas)
@@ -125,6 +122,7 @@ def mis_reservas():
 @app.route('/logout', methods = ['POST'])
 def logout():
     session.clear() 
+    
     return redirect('/')
 
 @app.route('/contact')
@@ -138,7 +136,7 @@ def admin():
     reservas = consultas.obtener_reservas()
     servicios = consultas.obtener_servicios()
     usuarios = consultas.obtener_usuarios()
-
+    
     return render_template('admin.html', hoteles=hoteles, habitaciones=habitaciones, reservas=reservas, servicios=servicios, usuarios=usuarios)
 
 @app.route('/admin/agregar_hotel', methods=['POST'])
@@ -146,24 +144,25 @@ def admin():
 def agregar_hotel():
     try:
         data = request.get_json()
+        
         if 'nombre' not in data:
             return jsonify({"error": "El campo 'nombre' es obligatorio"}), 400
-
+        
         nombre = data['nombre']
         descripcion = data['descripcion']
         ubicacion = data['ubicacion']
         hotel_id = consultas.agregar_hotel(nombre, descripcion, ubicacion)
-        
         imagenes = data.get('imagenesHotel', [])
+        
         if imagenes:
             consultas.agregar_imagenes(hotel_id, imagenes)
-
         nuevo_hotel = {"hotel_id": hotel_id, "nombre": nombre, "habilitado": 1}
+        
         return jsonify({"message": "Hotel agregado correctamente", "hotel": nuevo_hotel}), 201
-
+    
     except Exception as e:
-
         print(f"Error al agregar el hotel: {str(e)}")
+        
         return jsonify({"error": f"Error interno: {str(e)}"}), 500
 
 @app.route('/admin/deshabilitar_hotel/<int:hotel_id>', methods=['POST'])
@@ -224,9 +223,7 @@ def agregar_habitacion():
 
         capacidad = data['capacidad']
         hotel_id = data['hotel_id']
-
         habitacion_id = consultas.agregar_habitacion(capacidad, hotel_id)
-
         nueva_habitacion = {
             "id": habitacion_id,
             "capacidad": capacidad,
@@ -251,7 +248,6 @@ def agregar_servicio():
         url_imagen = data['url_imagen']
         ubicacion = data['ubicacion']
         categoria = data['categoria']
-
         servicio_id = consultas.agregar_servicio(nombre, descripcion, url_imagen, ubicacion, categoria);
 
         nuevo_servicio = {
@@ -278,15 +274,11 @@ def agregar_reserva():
         egreso = data.get('egreso')
         hotel_id = data.get('hotel_id')
         habitacion_id = data.get('habitacion_id')
-
         reserva_id = consultas.agregar_reserva(email, ingreso, egreso, hotel_id, habitacion_id)
         reserva = consultas.obtener_reseva_por_id(reserva_id)
-        
         appendear_reservas_session(reserva)
-
         enviar_correo(email, reserva_id, ingreso, egreso, hotel_id, habitacion_id)
         return jsonify({'success': True, 'message': 'Reserva realizada con éxito'}), 200
-
     except Exception as e:
         print(f"Error al crear la reserva: {str(e)}")
         return jsonify({"error": f"Error interno: {str(e)}"}), 500
@@ -353,37 +345,37 @@ def crear_reserva_servicio():
             return jsonify({"mensaje": "Reserva creada exitosamente"}), 201
         else:
             return jsonify({"error": "No se pudo crear la reserva"}), 500
+        
     except Exception as e:
         return {"error": f"Ocurrió un error: {str(e)}"}
-    
-
-
 
 @app.route('/admin/obtener_servicios_reserva/<int:id_reserva>', methods=['GET'])
 def obtener_servicios_reserva(id_reserva):
     try:
         servicios = consultas.obtener_servicios_por_reserva(id_reserva)
+        
         if servicios:
             return jsonify(servicios), 200
         else:
             return jsonify({"mensaje": "No se encontraron servicios para esta reserva"}), 404
+        
     except Exception as e:
         return jsonify({"error": f"Ocurrió un error: {e}"}), 500
-
 
 @app.route('/admin/obtener_reserva/<int:reservas_id>', methods=['GET'])
 def obtener_reserva(reservas_id):
     try:
         print(f"Buscando reserva con ID: {reservas_id}")  # Esto te ayuda a ver si la solicitud llega bien
         reserva = consultas.obtener_reserva_por_id(reservas_id)
+        
         if reserva:
             return jsonify(reserva), 200
         else:
             return jsonify({"error": "Reserva no encontrada"}), 404
+        
     except Exception as e:
         print(f"Error: {e}")  # Esto imprimirá el error en la consola de Flask
         return jsonify({"error": f"Ocurrió un error: {e}"}), 500
-
 
 @app.route('/admin/eliminar_servicio_reserva', methods=['DELETE'])
 def eliminar_servicio_reserva_endpoint():
@@ -406,8 +398,6 @@ def eliminar_servicio_reserva_endpoint():
     except Exception as e:
         print(f"Error al procesar la solicitud: {e}")
         return jsonify({"error": "Ocurrió un error interno"}), 500
-
-
 
 def enviar_correo(email, reserva_id, ingreso, egreso, hotel_id, habitacion_id):
     try:
@@ -479,7 +469,6 @@ def enviar_correo(email, reserva_id, ingreso, egreso, hotel_id, habitacion_id):
     except Exception as e:
         print(f"Error al enviar el correo: {str(e)}")
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  
+    app.run(host='0.0.0.0', port=5000)
 
